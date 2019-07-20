de8494381fc96726f9cab55b92994411c578cfaa
Warren Loy Harper II <warrenharper2@att.net>
2019-07-20 21:16:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,3 +2,4 @@ colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
 next_color = "violet"
 colors_in_the_rainbow.push(next_color)
 end
+bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
