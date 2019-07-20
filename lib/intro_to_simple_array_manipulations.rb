9dca322f5b1774ef7fc794f84504d651965ecfa7
Warren Loy Harper II <warrenharper2@att.net>
2019-07-20 21:21:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,3 +3,6 @@ next_color = "violet"
 colors_in_the_rainbow.push(next_color)
 end
 bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+new_neighborhood = "Staten Island"
+bouroughs_in_nyc.shift(new_neighborhood)
+end
