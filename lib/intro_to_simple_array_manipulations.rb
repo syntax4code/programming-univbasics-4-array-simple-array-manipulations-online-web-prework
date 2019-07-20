3248a8979139bd6902acab578646549c63e2cd4b
Warren Loy Harper II <warrenharper2@att.net>
2019-07-20 21:51:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -33,3 +33,5 @@ haircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
 haircuts.uniq
 end
 instruments = ["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"]
+instruments.flatten
+end
